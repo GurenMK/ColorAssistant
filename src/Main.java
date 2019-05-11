07e01e38f35ee49c734d433decdcb181d44bb6bb
Alexander Urbanyak <aurbanyak@gmail.com>
2019-05-11 14:37:06 -0400

    Update and rename ColorDsiplay.java to Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@
 
 import java.io.IOException;
 
-public class ColorDsiplay {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         new ColorAssistant("Color Assistant");
